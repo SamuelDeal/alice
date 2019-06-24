e39c97425955822b2f5786c93805c6ea4564b843
Samuel Déal <samuel.deal@gmail.com>
2019-06-24 19:46:23 +0200

    Initial commit

---
+++ malice/malice.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: set tabstop=4:softtabstop=4:shiftwidth=4:expandtab:textwidth=120
+
+"""
+This script will clean the project temporary files.
+It will:
+  * clean vagrant and docker virtual machines
+  * clean temporary files
+  * clean test certificates
+  * remove *.pyc files
+"""
+
+# Core libs
+import sys
+# import configparser
+# import os
+
+
+def main():
+    print("coucou")
+
+
+if __name__ == '__main__':
+    sys.exit(main())
